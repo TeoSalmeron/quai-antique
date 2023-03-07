d3269194933fb41990f416a7a4ccaf2428ffcf63
Téo Salmeron <teo.salmeron2601@gmail.com>
2023-03-08 00:20:17 +0100

    Added content in admin panel

--- Views/admin-panel/admin-panel.php
+++ Views/admin-panel/admin-panel.php
@@ -30,30 +30,86 @@
     <p>Dans cette section, vous pouvez modifier les horaires d'ouvertures et de service du restaurant, sa capacité d'accueil et son jour de fermeture.</p>
     <fieldset>
         <legend>Informations du restaurant</legend>
-        <div class="fieldset_item">
+        <form class="form_item" method="post" action="">
             <h3>Heure de début du service du midi</h3>
-            <p><?= $restaurant["noon_service_start"]?></p>
-        </div>
-        <div class="fieldset_item">
+            <p><?= $restaurant["noon_service_start"] ?></p>
+            <button class="edit_restaurant_info" id="edit1">Modifier</button>
+            <div class="edit_box" id="editBox1">
+                <input type="time" name="noon_service_start" id="noon_service_start">
+                <div class="btn_box">
+                    <button class="edit_box_btn success">Valider</button>
+                    <button type="reset" class="edit_box_btn cancel">Annuler</button>
+                </div>
+            </div>
+        </form>
+        <form class="form_item" method="post" action="">
             <h3>Heure de fin du service du midi</h3>
-            <p><?= $restaurant["noon_service_end"]?></p>
-        </div>
-        <div class="fieldset_item">
+            <p><?= $restaurant["noon_service_end"] ?></p>
+            <button class="edit_restaurant_info" id="edit2">Modifier</button>
+            <div class="edit_box" id="editBox2">
+                <input type="time" name="noon_service_end" id="noon_service_end">
+                <div class="btn_box">
+                    <button class="edit_box_btn success">Valider</button>
+                    <button type="reset" class="edit_box_btn cancel">Annuler</button>
+                </div>
+            </div>
+        </form>
+        <form class="form_item" method="post" action="">
             <h3>Heure de début du service du soir</h3>
-            <p><?=$restaurant["evening_service_start"]?></p>
-        </div>
-        <div class="fieldset_item">
+            <p><?= $restaurant["evening_service_start"] ?></p>
+            <button class="edit_restaurant_info" id="edit3">Modifier</button>
+            <div class="edit_box" id="editBox3">
+                <input type="time" name="evening_service_start" id="evening_service_start">
+                <div class="btn_box">
+                    <button class="edit_box_btn success">Valider</button>
+                    <button type="reset" class="edit_box_btn cancel">Annuler</button>
+                </div>
+            </div>
+        </form>
+        <form class="form_item" method="post" action="">
             <h3>Heure de fin du service du soir</h3>
-            <p><?=$restaurant["evening_service_end"]?></p>
-        </div>
-        <div class="fieldset_item">
+            <p><?= $restaurant["evening_service_end"] ?></p>
+            <button class="edit_restaurant_info" id="edit4">Modifier</button>
+            <div class="edit_box" id="editBox4">
+                <input type="time" name="evening_service_end" id="evening_service_end">
+                <div class="btn_box">
+                    <button class="edit_box_btn success">Valider</button>
+                    <button type="reset" class="edit_box_btn cancel">Annuler</button>
+                </div>
+            </div>
+        </form>
+        <form class="form_item" method="post" action="">
             <h3>Capacité maximale</h3>
-            <p><?=$restaurant["max_capacity"]?> couverts</p>
-        </div>
-        <div class="fieldset_item">
+            <p><?= $restaurant["max_capacity"] ?> couverts</p>
+            <button class="edit_restaurant_info" id="edit5">Modifier</button>
+            <div class="edit_box" id="editBox5">
+                <input type="number" name="max_capacity" id="max_capacity" min="1">
+                <div class="btn_box">
+                    <button class="edit_box_btn success">Valider</button>
+                    <button type="reset" class="edit_box_btn cancel">Annuler</button>
+                </div>
+            </div>
+        </form>
+        <form class="form_item form_item_borderless" method="post" action="">
             <h3>Jour de fermeture</h3>
             <p><?= ucfirst($day_close) ?></p>
-        </div>
+            <button class="edit_restaurant_info" id="edit6">Modifier</button>
+            <div class="edit_box" id="editBox6">
+                <select name="day_close" id="day_close">
+                    <option value="0">Dimanche</option>
+                    <option value="1">Luni</option>
+                    <option value="2">Mardi</option>
+                    <option value="3">Mercredi</option>
+                    <option value="4">Jeudi</option>
+                    <option value="5">Vendredi</option>
+                    <option value="6">Samedi</option>
+                </select>
+                <div class="btn_box">
+                    <button class="edit_box_btn success">Valider</button>
+                    <button type="reset" class="edit_box_btn cancel">Annuler</button>
+                </div>
+            </div>
+        </form>
     </fieldset>
 </section>
 
